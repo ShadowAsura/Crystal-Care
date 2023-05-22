0134d9a70196f17cf5738a06c153a6c961202c58
Bhargav Eranki <77950550+beranki@users.noreply.github.com>
2023-05-22 12:23:30 -0700

    website uwu

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,18 +1,19 @@
-<script>
-  import Navbar from "../components/Navbar.svelte";
-</script>
-
-<Navbar />
-
 <div class="wrapper"> 
     <img class="main_image" src="/images/crystal.png" alt="Crystal logo"/>    
-    <h1 class="title">crystal care</h1>
+    <h1><a href="/about" class="title">crystal care</a></h1>
 </div> 
 
 <div class="description">
-  <h3>Designed for Steel City Code Hacks 2023.</h3>
-  <h4>Bhargav</h4>
-  <h4>Shine</h4>
-  <h4>Taisei</h4>
-  <h4>Sriram</h4>
+  <h3>Designed for <b>Steel City Code Hacks 2023</b> by </h3>
+  <h4>Bhargav Eranki,</h4>
+  <h4>Shine Chang,</h4>
+  <h4>Taisei De La Cruz,</h4>
+  <h4>Sriram Loganathan.</h4>
+
+  <div class="button_holder">
+    <button type="button" class="btn btn-info">Login</button>
+    <button type="button" class="btn btn-info">Sign Up</button>
+  </div>
 </div>
+
+<!--make the shit scroll in uwu; click on the crystal and make it reroute to home page?-->
